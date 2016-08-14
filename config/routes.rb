6d1e1f9d232a439c0a22d39c3460411b9b40958e
Bradley <bradley.z1@hotmail.com>
2016-08-14 00:42:45 +0000

    Added contact page and made home the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-  
   get 'static_pages/about'
-
-  root 'application#hello'
+  get 'static_pages/contact'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
