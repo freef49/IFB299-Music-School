1c0e6cf2238ceed4fb53b18a9adf4632e5b061f0
Bradley <bradley.z1@hotmail.com>
2016-08-25 09:17:11 +0000

    Changed page to display different message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "This is the first step for the website!"
-  end
-  
-  def goodbye
-    render html: "Goodbye AmiGo¡"
+    render html: "This is the first step for the website! Building to test GitHub! Mistakes will be made!"
   end
 end
