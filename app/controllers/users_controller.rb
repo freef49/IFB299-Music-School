a9767f2d852df7d61ec84d175c3fc4f8c985c346
Bradley <bradley.z1@hotmail.com>
2016-09-03 05:45:40 +0000

    Reached 7.3.4. Sign up page and form nearly completed. Just requires tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :last_name, :dob, :email, :gender, :facebook_ID, :password,
+                                   :password_confirmation)
+    end
 end
