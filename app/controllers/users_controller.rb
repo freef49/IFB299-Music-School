f1c0640e28789ec364d3f3d36d2ce1ffe7527201
Bradley <bradley.z1@hotmail.com>
2016-09-03 10:23:49 +0000

    Users now auto log in on completeing the reg form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Mika Musical! Thanks for joining us :)"
       redirect_to @user # Handle a successful save.
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,6 +27,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
+
   end
 
 end
