9bd77a5b00e0df9995de57ab4801a52205bb934d
Bradley <bradley.z1@hotmail.com>
2016-09-03 13:48:46 +0000

    At chapter 10.1.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,35 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to Mika Musical! Thanks for joining us :)"
-      redirect_to @user # Handle a successful save.
+      flash[:success] = "Welcome to Mika Music!"
+      redirect_to @user
     else
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :last_name, :dob, :email, :gender, :facebook_ID, :password,
+      params.require(:user).permit(:name, :last_name, :dob, :email, :gender, :password,
                                    :password_confirmation)
     end
 end
