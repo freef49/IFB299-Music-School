5c67decd44c798860157736b2f3bb1e30367ece4
Bradley <bradley.z1@hotmail.com>
2016-10-15 04:02:57 +0000

    Fixed regex naming in availabilities model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -8,7 +8,7 @@ class Availability < ApplicationRecord
   VALID_DURATION_REGEX = /\A[0-9]{1}\.[0-9]{1}/
   
   validates :user_email,  presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
-  validates :day, presence: true, length: { maximum: 15 }, format: { with: VALID_DATE_REGEX }
+  validates :day, presence: true, length: { maximum: 15 }, format: { with: VALID_DAY_REGEX }
   validates :time, presence: true, length: {maximum: 6}, format: { with: VALID_TIME_REGEX }
   validates :duration, presence: true, length: { maximum: 4 }, format: { with: VALID_DURATION_REGEX }
   
