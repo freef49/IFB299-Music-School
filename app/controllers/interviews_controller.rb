e73f6fd51a4a603e1cd351114ba74d9b3e40020a
Bradley <bradley.z1@hotmail.com>
2016-10-15 07:13:23 +0000

    Added most of controller functionallity and mass assignment parameters

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,16 +1,36 @@
 class InterviewsController < ApplicationController
   def index
+    @interviews = Interview.all
   end
 
   def show
+    @interviews = Interview.find(params[:id])
   end
 
   def new
+    @interviews = Interview.new
+  end
+  
+  def create
   end
 
   def edit
+    @interviews = Interview.find(params[:id])
+  end
+  
+  def update
   end
 
   def delete
+    @interviews = Interview.find(params[:id])
+  end
+  
+  def destroy 
+  end
+  
+  private
+  
+  def interview_params
+    params.require(:interview).permit(:user_email, :teacher_email, :date, :time, :duration)
   end
 end
