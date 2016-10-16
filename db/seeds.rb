1c631fda774787e18f24ad2c8ce788027552ee30
Bradley <bradley.z1@hotmail.com>
2016-10-16 11:14:00 +0000

    Used ruby convention to remove if statments so code is much neater. Additionally, all student random input data is completed!

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,10 @@
 #
 # Seeds 5 completely random teachers (Each teacher has a random number of availabilities and skills)
 
+instrumentsAvailable = ["Guitar", "Drums", "Bass", "Claranet", "Vocal"]
+languagesAvailable = ["English", "Spanish", "Mandarin", "French", "Russian"]
+daysOpen = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
 user = User.create!(name:  "Mika",
               last_name: "Williams",
               dob: "11/09/1972",
@@ -80,18 +84,7 @@ Interview.create!(user_email: user.email,
                
   numAvailabilities.times do |a|
     # Variables for the Availability table of teacher
-    dayDecider = Faker::Number.between(1, 5)
-    if dayDecider == 1
-      day = "Monday"
-    elsif dayDecider == 2
-      day = "Tuesday"
-    elsif dayDecider == 3
-      day = "Wednesday"
-    elsif dayDecider == 4
-      day = "Thursday"
-    else
-      day = "Friday"
-    end
+    day = daysOpen[Faker::Number.between(0, 4)]
     time = "#{Faker::Number.between(10, 20)}:#{Faker::Number.between(10, 59)}"
     duration = Faker::Number.between(1, 5)
   
@@ -104,31 +97,9 @@ Interview.create!(user_email: user.email,
   
   numSkills.times do |b|
      # Variables for the Skills table of teacher
-    instrumentDecider = Faker::Number.between(1, 5)
-    if instrumentDecider == 1
-      instrument = "Piano"
-    elsif instrumentDecider == 2
-     instrument = "Guitar"
-    elsif instrumentDecider == 3
-      instrument = "Bass"
-    elsif instrumentDecider == 4
-      instrument = "Claranet"
-    else
-      instrument = "Drums"
-    end
+    instrument = instrumentsAvailable[Faker::Number.between(0, 4)]
     instrument_skill = "#{Faker::Number.between(1, 5)}.#{Faker::Number.between(1, 9)}"
-    languageDecider = Faker::Number.between(1, 5)
-    if languageDecider == 1
-      language = "English"
-    elsif languageDecider == 2
-      language = "Spanish"
-    elsif languageDecider == 3
-      language = "French"
-    elsif languageDecider == 4
-      language = "Mandarin"
-    else
-      language = "Latin"
-    end
+    language = languagesAvailable[Faker::Number.between(0, 4)]
     language_skill = "#{Faker::Number.between(1, 5)}.#{Faker::Number.between(1, 9)}"
   
     Skill.create!(user_email: user.email,
@@ -203,12 +174,34 @@ end
     time = "#{Faker::Number.between(10, 20)}:#{Faker::Number.between(10, 59)}"
     duration = Faker::Number.between(1, 5)
   
-    Availability.create!(user_email: user.email,
+    FreePlayRoom.create!(user_email: user.email,
                       user_id: user.id, 
                       date: date, 
                       time: time,
                       duration: duration)
   end
   
-  
+  numPreferences.times do |b|
+    # Variables for the preferences table of students
+    preferred_day = daysOpen[Faker::Number.between(0, 4)]
+    preferred_time = "#{Faker::Number.between(10, 20)}:#{Faker::Number.between(10, 59)}"
+    instrument = instrumentsAvailable[Faker::Number.between(0, 4)]
+    preferred_teacher_language = languagesAvailable[Faker::Number.between(0, 4)]
+    genderDecider = Faker::Number.between(1, 3)
+    if genderDecider == 1
+      preferred_teacher_gender = "Male"
+    elsif genderDecider == 2
+      preferred_teacher_gender = "Female"
+    else
+      preferred_teacher_gender = "No Preference"
+    end
+
+    Preference.create!(user_email: user.email,
+                      user_id: user.id, 
+                      preferred_day: preferred_day, 
+                      preferred_time: preferred_time,
+                      instrument: instrument, 
+                      preferred_teacher_language: preferred_teacher_language,
+                      preferred_teacher_gender: preferred_teacher_gender)
+  end
 end
