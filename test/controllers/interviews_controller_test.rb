3b6ed7f37636fcc0efb0f7b3c3eb06d4e043acac
Bradley <bradley.z1@hotmail.com>
2016-10-19 11:21:36 +0000

    Created test fixture for interviews and fixed errors on the interviews test controller

--- test/controllers/interviews_controller_test.rb
+++ test/controllers/interviews_controller_test.rb
@@ -6,6 +6,7 @@ class InterviewsControllerTest < ActionDispatch::IntegrationTest
     @admin = users(:mika)
     @teacher = users(:teacher1)
     @student = users(:student1)
+    @interview = interviews(:one)
   end
   
   test "should get index" do
@@ -22,13 +23,13 @@ class InterviewsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get edit" do
     log_in_as(@admin)
-    get edit_interview_path
+    get edit_interview_path(@interview)
     assert_response :success
   end
 
   test "should get delete" do
     log_in_as(@admin)
-    get delete_interview_path
+    get delete_interview_path(@interview)
     assert_response :success
   end
 
