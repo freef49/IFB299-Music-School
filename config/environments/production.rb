613b99066fb56211440ac98851a6598de29059f8
theothergame <theinversegame@gmail.com>
2016-10-20 07:08:33 +0000

    fixed what i hope to be a typo preventing emails form working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://ifb299-mikamusic-fm.herokuapp.com'
+  host = 'ifb299-mikamusic-fm.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
