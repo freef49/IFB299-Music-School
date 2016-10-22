744d5258e712ac923225238d729b2d3369d6b13c
Bradley <bradley.z1@hotmail.com>
2016-10-22 02:44:32 +0000

    Finished adding asserts to free play rooms and interviews controller

--- test/controllers/free_play_rooms_controller_test.rb
+++ test/controllers/free_play_rooms_controller_test.rb
@@ -2,25 +2,64 @@ require 'test_helper'
 
 class FreePlayRoomsControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index" do
+  test "student should get index" do
     log_in_as(@student1)
     get current_sessions_path
     assert_response :success
   end
-  test "should get new" do
+  test "student should get new" do
     log_in_as(@student1)
     get book_session_path
     assert_response :success
   end
-  test "should get edit" do
+  test "student should get edit" do
     log_in_as(@student1)
     get edit_free_play_room_path(@free_play_room1)
     assert_response :success
   end
-  test "should get delete" do
+  test "student should get delete" do
     log_in_as(@student1)
     get delete_free_play_room_path(@free_play_room1)
     assert_response :success
   end
+  
+  
+  test "teacher should get index" do
+    log_in_as(@teacher1)
+    get current_sessions_path
+    assert_response :success
+  end
+  test "teacher should not get new" do
+    log_in_as(@teacher1)
+    get book_session_path
+    assert_redirected_to root_url
+  end
+  test "teacher should not get edit" do
+    log_in_as(@teacher1)
+    get edit_free_play_room_path(@free_play_room1)
+    assert_redirected_to root_url
+  end
+  test "teacher should not get delete" do
+    log_in_as(@teacher1)
+    get delete_free_play_room_path(@free_play_room1)
+    assert_redirected_to root_url
+  end
 
+
+  test "unlogged user should not get index" do
+    get current_sessions_path
+    assert_redirected_to login_url
+  end
+  test "unlogged user should not get new" do
+    get book_session_path
+    assert_redirected_to login_url
+  end
+  test "unlogged user should not get edit" do
+    get edit_free_play_room_path(@free_play_room1)
+    assert_redirected_to login_url
+  end
+  test "unlogged user should not get delete" do
+    get delete_free_play_room_path(@free_play_room1)
+    assert_redirected_to login_url
+  end
 end
