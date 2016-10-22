10d0eb7e52eebb17a27216accc98193f208e4274
Bradley <bradley.z1@hotmail.com>
2016-10-22 02:32:24 +0000

    Moved the setup function for test controllers into test helper. This stops myself from having to retype the same code for each controller

--- test/controllers/free_play_rooms_controller_test.rb
+++ test/controllers/free_play_rooms_controller_test.rb
@@ -1,29 +1,26 @@
 require 'test_helper'
 
 class FreePlayRoomsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get current_sessions_path
-  #   assert_response :success
-  # end
 
-  # test "should get show" do
-  #   get free_play_rooms_path
-  #   assert_response :success
-  # end
-
-  # test "should get new" do
-  #   get book_session_path
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get free_play_rooms_path
-  #   assert_response :success
-  # end
-
-  # test "should get delete" do
-  #   get free_play_rooms_path
-  #   assert_response :success
-  # end
+  test "should get index" do
+    log_in_as(@student1)
+    get current_sessions_path
+    assert_response :success
+  end
+  test "should get new" do
+    log_in_as(@student1)
+    get book_session_path
+    assert_response :success
+  end
+  test "should get edit" do
+    log_in_as(@student1)
+    get edit_free_play_room_path(@free_play_room1)
+    assert_response :success
+  end
+  test "should get delete" do
+    log_in_as(@student1)
+    get delete_free_play_room_path(@free_play_room1)
+    assert_response :success
+  end
 
 end
