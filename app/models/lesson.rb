c9ed2770cde62b0efa5c6388e8bb95e1316117e3
theothergame <theinversegame@gmail.com>
2016-10-23 12:10:33 +0000

    added validation to lessons model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,14 @@
 class Lesson < ApplicationRecord
   belongs_to :user
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_DATE_REGEX = /\A(?:0?[1-9]|[1-2]\d|3[01])\/(?:0?[1-9]|1[0-2])\/\d{4}\Z/
+  VALID_TIME_REGEX = /\A^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/
+
+  validates :user_email,  presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
+  validates :start_date, presence:true, format: { with: VALID_DATE_REGEX }
+  validates :end_date, presence:true, format: { with: VALID_DATE_REGEX }
+  validates :lesson_time, presence: true, format: { with: VALID_TIME_REGEX }
+   
+
 end
