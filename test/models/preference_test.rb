36cb738253c177cf12345c574fff389ed0d4e19a
Bradley <bradley.z1@hotmail.com>
2016-10-26 01:37:31 +0000

    Finished adding model tests for all controllers created by myself: availabilitiy, free_play_room, interview, preference and skill

--- test/models/preference_test.rb
+++ test/models/preference_test.rb
@@ -1,7 +1,68 @@
 require 'test_helper'
 
 class PreferenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @preference = Preference.new(user_id: "1", user_email: "student@example.gov", preferred_day: "Monday",
+                                preferred_time: "12:30", instrument: "Guitar", 
+                                preferred_teacher_language: "English", preferred_teacher_gender: "Male")
+  end
+  
+  test "Should be Valid" do
+     assert @preference.valid?
+  end
+  
+  test "user_id should be required" do
+    @preference.user_id = "     "
+    assert_not @preference.valid?
+  end
+  test "user_email should be required" do
+    @preference.user_email = "     "
+    assert_not @preference.valid?
+  end
+  test "preferred_day should be required" do
+    @preference.preferred_day = "     "
+    assert_not @preference.valid?
+  end
+  test "preferred_time should be required" do
+    @preference.preferred_time = "     "
+    assert_not @preference.valid?
+  end
+  test "instrument should be required" do
+    @preference.instrument = "     "
+    assert_not @preference.valid?
+  end
+  test "preferred_teacher_language should be required" do
+    @preference.preferred_teacher_language = "     "
+    assert_not @preference.valid?
+  end
+  test "preferred_teacher_gender should be required" do
+    @preference.preferred_teacher_gender = "     "
+    assert_not @preference.valid?
+  end
+  
+  test "user_id should be a valid number" do
+    @preference.user_id = "Cat"
+    assert_not @preference.valid?
+  end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @preference.user_email = invalid_address
+      assert_not @preference.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  test "preferred_day should be string" do
+    @preference.preferred_day = "401"
+    assert_not @preference.valid?
+  end
+  test "preferred_time should be correct format" do
+    @preference.preferred_time = "12/50"
+    assert_not @preference.valid?
+  end
+  test "preferred_teacher_gender should be string" do
+    @preference.preferred_teacher_gender = "1"
+    assert_not @preference.valid?
+  end
 end
--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class SkillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @skill = Skill.new(user_id: "1", user_email: "teachernum1@example.gov", instrument: "Guitar",
+                        instrument_skill: "2.5", language: "English", language_skill: "4.5")
+  end
+  
+  test "Should be Valid" do
+     assert @skill.valid?
+  end
+  
+  test "user_id should be required" do
+    @skill.user_id = "     "
+    assert_not @skill.valid?
+  end
+  test "user_email should be required" do
+    @skill.user_email = "     "
+    assert_not @skill.valid?
+  end
+  test "instrument should be required" do
+    @skill.instrument = "     "
+    assert_not @skill.valid?
+  end
+  test "instrument_skill should be required" do
+    @skill.instrument_skill = "     "
+    assert_not @skill.valid?
+  end
+  test "language should be required" do
+    @skill.language = "     "
+    assert_not @skill.valid?
+  end
+  test "language_skill should be required" do
+    @skill.language_skill = "     "
+    assert_not @skill.valid?
+  end
+  
+  test "user_id should be a valid number" do
+    @skill.user_id = "Cat"
+    assert_not @skill.valid?
+  end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @skill.user_email = invalid_address
+      assert_not @skill.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  test "instrument should be string" do
+    @skill.instrument = "401"
+    assert_not @skill.valid?
+  end
+  test "instrument_skill should be between 1 and 5" do
+    @skill.instrument_skill = "6.5"
+    assert_not @skill.valid?
+  end
+  test "instrument_skill should be valid between 1 and 5" do
+    @skill.instrument_skill = "1.5"
+    assert @skill.valid?
+  end
+  test "language should be string" do
+    @skill.language = "401"
+    assert_not @skill.valid?
+  end
+  test "language_skill should be between 1 and 5" do
+    @skill.language_skill = "-0.5"
+    assert_not @skill.valid?
+  end
 end
